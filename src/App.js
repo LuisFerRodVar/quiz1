88ac1acb2593225cb4de093ed5778e95676fb974
LuisFerRodVar <luisfrv1@estudiantec.cr>
2024-04-11 18:38:23 -0600

    Segundo ejercicio completado

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState("");
   const [likes, setLikes] = useState(0);
   function modificarLikes(operacion){
     setLikes(likes + operacion);
   }
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
   return (
     <> 
       <h1 className='title'>Cantidad de likes: {likes}</h1>
@@ -13,6 +17,8 @@ function App() {
         <span onClick={() => modificarLikes(1)} className="material-symbols-outlined">thumb_up</span>
         <span onClick={() => modificarLikes(-1)} className="material-symbols-outlined">thumb_down</span> 
       </div>
+      <h2 className='title2'>Su nombre es: {name} </h2>
+      <input onChange={handleChange} type='text' placeholder='nombre' />
     
     </>
 
