19dc1bd03b03312dc4177c0b9ff367bdef097cf5
LuisFerRodVar <luisfrv1@estudiantec.cr>
2024-04-11 18:32:54 -0600

    Agregada actividad 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [likes, setLikes] = useState(0);
+  function modificarLikes(operacion){
+    setLikes(likes + operacion);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+      <h1 className='title'>Cantidad de likes: {likes}</h1>
+      <div className='panel__review'>
+        <span onClick={() => modificarLikes(1)} className="material-symbols-outlined">thumb_up</span>
+        <span onClick={() => modificarLikes(-1)} className="material-symbols-outlined">thumb_down</span> 
+      </div>
+    
+    </>
+
   );
 }
 
